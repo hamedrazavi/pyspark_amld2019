6856d4efd64ec7242e434fdafe380c99a0ab8ba0
Hamed Razavi <razavi@umich.edu>
2019-01-23 18:04:50 +0100

    updated emr configs, and aws readme

---
+++ emr_bootstrap/emr_configs.sh
@@ -0,0 +1,38 @@
+# ----------------------------------------------------------------------
+#  move /usr/local to /mnt/usr-moved/local; else run out of space on /
+# ----------------------------------------------------------------------
+sudo mkdir /mnt/usr-moved
+sudo mv /usr/local /mnt/usr-moved/
+sudo ln -s /mnt/usr-moved/local /usr/
+sudo mv /usr/share /mnt/usr-moved/
+sudo ln -s /mnt/usr-moved/share /usr/
+
+# ----------------------------------------------------------------------
+#              Install Anaconda (Python 3) & Set To Default              
+# ----------------------------------------------------------------------
+echo installing Anaconda...
+wget https://repo.continuum.io/archive/Anaconda3-2018.12-Linux-x86_64.sh -O ~/anaconda.sh
+bash ~/anaconda.sh -b -p $HOME/anaconda
+echo -e '\nexport PATH=$HOME/anaconda/bin:$PATH' >> $HOME/.bashrc 
+source $HOME/.bashrc
+
+#-----------------------------------------------------------------------
+# 		Create a conda environment
+#-----------------------------------------------------------------------
+echo creating conda environment...
+conda create -yn py36 python=3.6
+
+#-----------------------------------------------------------------------
+# 		Install jupyter in the conda environment
+#-----------------------------------------------------------------------
+conda install -yn py36 -c anaconda jupyter
+
+
+#-----------------------------------------------------------------------
+# 		Set PySpark environments
+#-----------------------------------------------------------------------
+echo -e '\nexport PYSPARK_PYTHON=~/anaconda/envs/py36/bin/python' >> $HOME/.bashrc 
+echo -e '\nexport PYSPARK_DRIVER_PYTHON=~/anaconda/envs/py36/bin/jupyter' >> $HOME/.bashrc 
+echo -e '\nexport PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=8889"' >> $HOME/.bashrc 
+source $HOME/.bashrc
+
--- env_var_settings.sh
+++ env_var_settings.sh
@@ -1,5 +1,5 @@
-export PYSPARK_PYTHON=/data/srazavi/bin/anaconda3/envs/envPy36/bin/python
+export PYSPARK_PYTHON=`which python`
 #export PYSPARK_PYTHON=/anaconda3/envs/envPy36/bin/python
-export PYSPARK_DRIVER_PYTHON=/data/srazavi/bin/anaconda3/envs/envPy36/bin/jupyter
+export PYSPARK_DRIVER_PYTHON=`which jupyter`
 #export PYSPARK_DRIVER_PYTHON=/anaconda3/envs/envPy36/bin/jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS='lab'
